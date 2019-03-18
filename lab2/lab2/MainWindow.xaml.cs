82276a3de643585a3db10ea9a8b6c12a50fd5c07
Kamil Szprych <kkamil4sz@gmail.com>
2019-03-19 00:54:39 +0100

    Dodano zczytywanie wszystkich danych pogodowych oraz zaimplementowano klasę do ich trzymania

--- lab2/lab2/MainWindow.xaml.cs
+++ lab2/lab2/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace lab2
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        WeatherData weatherData;
         static async Task<string> StringDownload()
         {
             string responseBody = string.Empty;
@@ -30,7 +30,7 @@ namespace lab2
             {
                 try
                 {
-                    responseBody = await client.GetStringAsync("https://www.twojapogoda.pl/prognoza-polska/dolnoslaskie-wroclaw/");
+                    responseBody = await client.GetStringAsync("https://pogoda.interia.pl/prognoza-szczegolowa-wroclaw,cId,39240");
                 }
                 catch(HttpRequestException e)
                 {
@@ -55,10 +55,23 @@ namespace lab2
             var htmlDocument = new HtmlDocument();
             htmlDocument.LoadHtml(await resultTask);
 
-            var parsedList = htmlDocument.DocumentNode.Descendants("span")
-                .Where(node => node.GetAttributeValue("class", "").Equals("json-temperature")).ToList();
+            //Jedna z metod poruszania się po nodach (znacznikach) w html z użyciem biblioteki HtmlAgilityPack
+            var weatherTemperature = htmlDocument.DocumentNode.Descendants("div")
+                .Where(node => node.GetAttributeValue("class", "").Equals("weather-currently-temp-strict")).First().InnerText;
 
-            downloadedDataBox.Text = await resultTask;
+            //Druga metoda za pomocą ścieżki xPath również obsługiwanej przez HtmlAgilityPack
+            var weatherState = htmlDocument.DocumentNode.SelectNodes("//li[@class='weather-currently-icon-description']").First().InnerText.Trim();
+
+            var weatherTemperatureFelt = htmlDocument.DocumentNode.SelectSingleNode("//li[@class='weather-currently-details-item feelTemperature']/span").InnerText;
+            var weatherPressure = htmlDocument.DocumentNode.SelectSingleNode("//li[@class='weather-currently-details-item pressure']/span").InnerText.Replace(" ", "");
+            var weatherWind = htmlDocument.DocumentNode.SelectSingleNode("//li[@class='weather-currently-details-item wind']/span").InnerText.Replace(" ", "");
+
+            weatherData = new WeatherData(weatherTemperature, weatherTemperatureFelt, weatherPressure, weatherWind, weatherState);
+
+            downloadedDataBox.Text = weatherData + "\n";
+            downloadedDataBox.Text += weatherState + "\n";
+            downloadedDataBox.Text += weatherPressure + "\n";
+            downloadedDataBox.Text += weatherWind + "\n";
         }
     }
 }
---
+++ lab2/lab2/WeatherData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace lab2
+{
+    public class WeatherData
+    {
+        public WeatherData(string setTemperature, string setTemperatureFelt, string setPressure, string setWind, string setState)
+        {
+            temperature = int.Parse(Regex.Replace(setTemperature, "[^0-9]", ""));
+            temperatureFelt = int.Parse(Regex.Replace(setTemperatureFelt, "[^0-9]", ""));
+            pressure = int.Parse(Regex.Replace(setPressure, "[^0-9]", ""));
+            wind = int.Parse(Regex.Replace(setWind, "[^0-9]", ""));
+            state = setState;
+        }
+
+        int temperature;
+        int temperatureFelt;
+        int pressure;
+        int wind;
+        string state;
+    }
+}
