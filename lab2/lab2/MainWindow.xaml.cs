e234664be9fcb3206257a0aebb528fff33cf02df
Kamil Szprych <kkamil4sz@gmail.com>
2019-03-20 01:13:28 +0100

    Dodano pełny interfejs graficzny oraz animację ładowania

--- lab2/lab2/MainWindow.xaml.cs
+++ lab2/lab2/MainWindow.xaml.cs
@@ -42,14 +42,9 @@ namespace lab2
             return responseBody;
         }
 
-
-        public MainWindow()
+        async Task GetWeatherDataFromString()
         {
-            InitializeComponent();
-        }
-
-        private async void Download_Click(object sender, RoutedEventArgs e)
-        {
+            loadingImage.Visibility = Visibility.Visible;
             var resultTask = StringDownload();
 
             var htmlDocument = new HtmlDocument();
@@ -60,31 +55,56 @@ namespace lab2
                 .Where(node => node.GetAttributeValue("class", "").Equals("temp")).First().InnerText;
 
             //Druga metoda za pomocą ścieżki xPath również obsługiwanej przez HtmlAgilityPack
-            var weatherState = htmlDocument.DocumentNode.SelectNodes("//div[@class='forecastDesc']").First().InnerText.Trim();
-            var weatherTemperatureFelt = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='feelTempValue']").InnerText;
-
+            var weatherState = htmlDocument.DocumentNode
+                .SelectNodes("//div[@class='forecastDesc']").First().InnerText.Trim();
+            var weatherTemperatureFelt = htmlDocument.DocumentNode
+                .SelectSingleNode("//span[@class='feelTempValue']").InnerText;
             var weatherOtherParameters = htmlDocument.DocumentNode
                 .SelectNodes("//div[@class='weatherParams']/ul/li/span[@class='restParamValue']").ToList();
+            var weatherIcon = htmlDocument.DocumentNode
+                .SelectSingleNode("//div[@class='forecast']/span[@class='iconHolder']/img").GetAttributeValue("src", "");
 
             var weatherRain = weatherOtherParameters[0].InnerText;
             var weatherWind = weatherOtherParameters[1].InnerText;
             var weatherPressure = weatherOtherParameters[2].InnerText;
             var weatherClouds = weatherOtherParameters[3].InnerText;
             var weatherHumidity = weatherOtherParameters[4].InnerText;
-            //var weatherPressure = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='weatherParams']/span").InnerText.Replace(" ", "");
-            //var weatherWind = htmlDocument.DocumentNode.SelectSingleNode("//li[@class='weather-currently-details-item wind']/span").InnerText.Replace(" ", "");
-            ////var weatherIcon = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='weather-currently-icon ico-33']");
 
-            weatherData = new WeatherData(weatherTemperature, weatherTemperatureFelt, weatherPressure, weatherWind, weatherHumidity, weatherRain, weatherClouds, weatherState);
 
-            downloadedDataBox.Text = "Temperatura: " + weatherData.Temperature + "\n";
-            downloadedDataBox.Text += "Temperatura odczuwalna: " + weatherData.TemperatureFelt + "\n";
-            downloadedDataBox.Text += "Opis: " + weatherData.State + "\n";
-            downloadedDataBox.Text += "Ciśnienie: " + weatherData.Pressure + " hPa\n";
-            downloadedDataBox.Text += "Prędkość wiatru: " + weatherData.Wind + " km/h\n";
-            downloadedDataBox.Text += "Wilgotność: " + weatherData.Humidity + " %\n";
-            downloadedDataBox.Text += "Zachmurzenie: " + weatherData.Clouds + " %\n";
-            downloadedDataBox.Text += "Deszcz: " + weatherData.Rain + " mm\n";
+            weatherData = new WeatherData(weatherTemperature, weatherTemperatureFelt, weatherPressure, weatherWind, weatherHumidity, weatherRain, weatherClouds, weatherState, weatherIcon);
+            loadingImage.Visibility = Visibility.Collapsed;
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Download_Click(object sender, RoutedEventArgs e)
+        {
+            await GetWeatherDataFromString();
+            download.Margin = new Thickness(0, 359, 0, 0);
+            mainWeatherDataGrid.Visibility = Visibility.Visible;
+            additionaWeatherDataGrid.Visibility = Visibility.Visible;
+            temperatureDisplay.Text = weatherData.Temperature + " °C";
+            temperatureFeltDisplay.Text = weatherData.TemperatureFelt + " °C";
+            stateDisplay.Text = weatherData.State;
+            windDisplay.Text = weatherData.Wind + " km/h";
+            pressureDisplay.Text = weatherData.Pressure + " hPa";
+            humidityDisplay.Text = weatherData.Humidity + " %";
+            cloudDisplay.Text = weatherData.Clouds + " %";
+            rainDisplay.Text = weatherData.Rain + " mm";
+            iconDisplay.Source = new BitmapImage(new Uri(weatherData.IconLink));
+
+            //downloadedDataBox.Text = "Temperatura: " + weatherData.Temperature + "\n";
+            //downloadedDataBox.Text += "Temperatura odczuwalna: " + weatherData.TemperatureFelt + "\n";
+            //downloadedDataBox.Text += "Opis: " + weatherData.State + "\n";
+            //downloadedDataBox.Text += "Ciśnienie: " + weatherData.Pressure + " hPa\n";
+            //downloadedDataBox.Text += "Prędkość wiatru: " + weatherData.Wind + " km/h\n";
+            //downloadedDataBox.Text += "Wilgotność: " + weatherData.Humidity + " %\n";
+            //downloadedDataBox.Text += "Zachmurzenie: " + weatherData.Clouds + " %\n";
+            //downloadedDataBox.Text += "Deszcz: " + weatherData.Rain + " mm\n";
         }
     }
 }
--- lab2/lab2/WeatherData.cs
+++ lab2/lab2/WeatherData.cs
@@ -9,7 +9,7 @@ namespace lab2
 {
     public class WeatherData
     {
-        public WeatherData(string setTemperature, string setTemperatureFelt, string setPressure, string setWind, string setHumidity, string setRain, string setClouds, string setState)
+        public WeatherData(string setTemperature, string setTemperatureFelt, string setPressure, string setWind, string setHumidity, string setRain, string setClouds, string setState, string setIconLink )
         {
             Temperature = int.Parse(Regex.Replace(setTemperature, "[^-0-9]", ""));
             TemperatureFelt = int.Parse(Regex.Replace(setTemperatureFelt, "[^-0-9]", ""));
@@ -18,7 +18,13 @@ namespace lab2
             Humidity = int.Parse(Regex.Replace(setHumidity, "[^0-9]", ""));
             Rain = int.Parse(Regex.Replace(setRain, "[^0-9]", ""));
             Clouds = int.Parse(Regex.Replace(setClouds, "[^0-9]", ""));
+
             State = setState;
+            if (setIconLink.StartsWith("http:") || setIconLink.StartsWith("https:"))
+                IconLink = setIconLink;
+            else
+                IconLink = "https:" + setIconLink;
+            //BackgroundLink = setBackgroundLink;
         }
 
         public int Temperature { get; }
@@ -29,5 +35,7 @@ namespace lab2
         public int Rain { get; }
         public int Clouds { get; }
         public string State { get; }
+        public string IconLink { get; }
+        //public string BackgroundLink { get; }
     }
 }
